90c2c27c3c17dddbbfb8c560452f06485c0e9e9a
YONDE927 <svyuta1202@gmail.com>
2018-10-26 12:49:06 +0000

    make views

--- item/views.py
+++ item/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template.response import TemplateResponse
-
+from django.contrib.auth.decorators import login_required
+from .models import Item
 # Create your views here.
 def hello(request):
     context={
@@ -23,3 +24,50 @@ def post(request,post_id):
 
 def news(request,slug):
     return HttpResponse('slugは = {}です。'.format(slug))
+
+@login_required
+def edit(request,item_id):
+    item=get_objiect_or_404(Item,id=item_id)
+    if request.method is 'POST':
+        form=ItemForm(request.POST,instance=item)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('item_index'))
+    else:
+        form=ItemForm(instance=item)
+    context={"form":form,"item":item}
+    TemplateResponse(request,'item/edit.html',context=context)
+
+@login_required
+@require_POST
+def delete(request,item_id):
+    item=get_objiect_or_404(Item,id=item_id)
+    item.delete()
+    return HttpResponse(reverse('item_index'))
+
+@login_required
+def index(request):
+    context={'items':Item.objects.all()}
+    return TemplateResponse(request,'item/list.html',context=context)
+
+@login_required
+@require_POST
+def add_to_wish_list(request,item_id):
+    item = get_object_or_404(item, id=item_id)
+    wish_list, created=WishList.object.get_or_create(user=request.user)
+    wish_list.items.add(item)
+    return HttpResponseRedirecti(reverse('wish_list_index'))
+
+@login_required
+@require_POST
+def delete_form_wish_list(request, item_id):
+    item = get_object_or_404(item,id=item_id)
+    wish_list, created= WishList.objects.get_or_create(user=request.user)
+    wish_list.items.remove(item)
+    return HttpResponseRedirect(reverse('wish_list_index'))
+
+@login_required
+def wish_list_index(request):
+    wish_list, created = WishList.objects.get_or_create(user=request.user)
+    context={'items': wish_list.items.all()}
+    return TemplateResponse(request, 'wish_list/list.html', context=context)
